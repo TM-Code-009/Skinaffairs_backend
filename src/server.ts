daea68a2e3aee82a751ed6dfec0c3e03cef35c16
TM-Code-009 <osakweterrynduka@gmail.com>
2025-04-11 21:39:04 +0100

    cors updated

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", // Allow frontend
-    methods: "GET,POST,PUT,DELETE",
+    origin: "*",
     credentials: true
   }));
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+ 
 
 connectDB();
 
